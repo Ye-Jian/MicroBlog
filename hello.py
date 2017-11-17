3d4d6c382515044928e36e7a55b93a5bbfde87b3
Ye Jian <YeJian1024@foxmail.com>
2017-11-17 19:44:24 +0800

    集成Jinja2模板引擎

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -9,13 +9,13 @@ manager = Manager(app)
 # 将函数index注册为应用根url的处理程序：
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 # 添加一个动态路由：
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 
 # 启动Flask集成的开发Web服务器：
