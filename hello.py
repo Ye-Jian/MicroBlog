a4983c41c072348d06c7afec49dfebe31d743cb8
Ye Jian <YeJian1024@foxmail.com>
2017-11-17 13:37:57 +0800

    添加一个动态路由

--- hello.py
+++ hello.py
@@ -9,6 +9,12 @@ def index():
     return '<h1>Hello World!</h1>'
 
 
+# 添加一个动态路由：
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
 # 启动Flask集成的开发Web服务器：
 if __name__ == '__main__':
     app.run(debug=True)
