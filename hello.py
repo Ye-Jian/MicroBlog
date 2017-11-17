fbea9f415b92a28f1410209b25a8ef7c02f0f78d
Ye Jian <YeJian1024@foxmail.com>
2017-11-17 20:41:29 +0800

    自定义错误页面

--- hello.py
+++ hello.py
@@ -8,6 +8,18 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 
 
+# 404错误处理：
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# 500错误处理：
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 # 将函数index注册为应用根url的处理程序：
 @app.route('/')
 def index():
